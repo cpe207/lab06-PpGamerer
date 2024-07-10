739b150e79b85e1e3a5d9030c8234a30d52ffed6
Pp <gamerer16@gmail.com>
2024-07-10 15:06:25 +0700

    updated

--- q1.ts
+++ q1.ts
@@ -3,7 +3,13 @@ import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getUser = async (userId: number) => {
-  /* Your code here */
+  try {
+    let link:string = "https://jsonplaceholder.typicode.com/users/" + userId;
+    const res = await axios.get(link)
+    return res.data.name;
+    } catch(err) {
+          return "INVALID USER ID";
+      }
 };
 
 //test case
@@ -16,3 +22,9 @@ getUser(input2).then((result) => console.log(result));
 
 // module.exports = getUser;
 export default getUser;
+
+/*
+รหัส นศ.: 660610779
+
+ชื่อ-สกุล : พิมลนาฏ แก้วบุตร
+*/
--- q2.ts
+++ q2.ts
@@ -3,7 +3,23 @@ import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
-  /* Your code here */
+  try {
+    let link1:string = "https://jsonplaceholder.typicode.com/todos/" + todoId;
+    const todores = await axios.get(link1)
+    const todo = todores.data;
+    const userId:number = todores.data.userId;
+
+    let link2:string = "https://jsonplaceholder.typicode.com/users/" + userId;
+    const userres = await axios.get(link2)
+    const user = userres.data;
+    return {
+          owner: user.name,
+          title: todo.title,
+          completed: todo.completed
+        };
+    } catch(err) {
+          return "INVALID TODO ID";
+      }
 };
 
 //test case
@@ -17,3 +33,9 @@ getTodo(input2).then((result) => console.log(result));
 getTodo(input3).then((result) => console.log(result));
 
 export default getTodo;
+
+/*
+รหัส นศ.: 660610779
+
+ชื่อ-สกุล : พิมลนาฏ แก้วบุตร
+*/
